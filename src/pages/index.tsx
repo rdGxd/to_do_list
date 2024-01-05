987af02ee6c60bc7d2fd49e20ebb27b2db8d5497
Rodrigo Silva <rdg.hora1@gmail.com>
2024-01-05 11:39:10 -0300

    refactor: removendo o component Login e deixando o component Home como padrão

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Login } from "@/templates/Login";
+import { Home } from "@/templates/Home";
 import { useSession } from "next-auth/react";
 
 export default function IndexPage() {
@@ -7,7 +7,7 @@ export default function IndexPage() {
   return (
     <>
       <h1>{session && `${session.user?.email} ${session.user?.name} `}</h1>
-      <Login />
+      <Home />
     </>
   );
 }
--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,7 +1,56 @@
+import { githubIcon, googleIcon } from "@/assets";
+import { Button } from "@/components";
+import { signIn, useSession } from "next-auth/react";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 export const Home = () => {
+  const { data: session } = useSession();
+  const { push } = useRouter();
+
+  useEffect(() => {
+    if (session?.user) {
+      push("/create");
+    }
+  }, [session?.user, push]);
+
+  const handleLoginGoogle = async () => {
+    await signIn("google", { callbackUrl: "/create" });
+  };
+
+  const handleLoginGithub = async () => {
+    await signIn("github", { callbackUrl: "/create" });
+  };
+
   return (
-    <>
-      <h1>HOME</h1>
-    </>
+    <main className="flex flex-wrap content-center items-center justify-center text-center">
+      <div className="mt-10 flex flex-wrap items-center justify-center">
+        <h1 className="text-2xl font-bold text-blue-950">Oi, Bem vindo</h1>
+        <span className="p-2 text-xs text-gray-500">
+          Faça login na sua conta para começar a aproveitar a sua To do List
+        </span>
+      </div>
+      <section>
+        {/* Google LOGIN */}
+        <div className="GOOGLE  mt-5 flex w-fit justify-center rounded border p-4">
+          <Image src={googleIcon} alt="Icon Google" className="mr-2" />
+          <Button
+            text="Faça login no Google"
+            type="button"
+            onClick={handleLoginGoogle}
+          />
+        </div>
+        {/* Github LOGIN */}
+        <section className="GITHUB  mt-5 flex w-fit justify-center rounded border p-4">
+          <Image src={githubIcon} alt="Icon GitHub" className="mr-2" />
+          <Button
+            text="Faça login no GitHub"
+            type="button"
+            onClick={handleLoginGithub}
+          />
+        </section>
+      </section>
+    </main>
   );
 };
