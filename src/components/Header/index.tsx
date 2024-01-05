b2b627d05df93a0aefe2407c4334badb0b8a3933
Rodrigo Silva <rdg.hora1@gmail.com>
2024-01-05 11:38:15 -0300

    feat: MenuMobile header quase finalizado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { MenuMobileIcon } from "@/assets";
+import { CloseMenuMobileIcon, MenuMobileIcon } from "@/assets";
 import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import { useRouter } from "next/router";
@@ -7,7 +7,7 @@ import { Button } from "..";
 
 export const Header = () => {
   const { data: session } = useSession();
-  const [open, setOpen] = useState(false);
+  const [mobileMenu, setMobileMenu] = useState(false);
   const router = useRouter();
 
   const handleLogout = async () => {
@@ -19,23 +19,48 @@ export const Header = () => {
       <header>
         {session?.user && (
           <>
-            {open ? "" : ""}
-            <div className="MOBILE flex items-center justify-around p-2">
-              <Image
-                src={MenuMobileIcon}
-                alt="Menu mobile Icon"
-                onClick={() => setOpen(!open)}
-              />
-              <Button text="Desconectar" type="button" onClick={handleLogout} />
-              <Image
-                src={session.user.image as string}
-                alt="Menu mobile Icon"
-                width={40}
-                height={40}
-                className="rounded-full"
-                onClick={() => router.push("/create")}
-              />
-            </div>
+            {mobileMenu ? (
+              <>
+                <div className="min-w-screen flex min-h-screen items-center justify-around">
+                  <Image
+                    alt="Close Menu"
+                    src={CloseMenuMobileIcon}
+                    className="h-10 w-8"
+                    onClick={() => setMobileMenu(!mobileMenu)}
+                  />
+                  <h2 className="font-bold ">To do List</h2>
+                  <Image
+                    src={session.user.image as string}
+                    alt="Menu mobile Icon"
+                    width={40}
+                    height={40}
+                    className="rounded-full"
+                    onClick={() => router.push("/create")}
+                  />
+                </div>
+              </>
+            ) : (
+              <div className="MOBILE flex items-center justify-around p-2">
+                <Image
+                  src={MenuMobileIcon}
+                  alt="Menu mobile Icon"
+                  onClick={() => setMobileMenu(!mobileMenu)}
+                />
+                <Button
+                  text="Desconectar"
+                  type="button"
+                  onClick={handleLogout}
+                />
+                <Image
+                  src={session.user.image as string}
+                  alt="Menu mobile Icon"
+                  width={40}
+                  height={40}
+                  className="rounded-full"
+                  onClick={() => router.push("/create")}
+                />
+              </div>
+            )}
           </>
         )}
       </header>
